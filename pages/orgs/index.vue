00ece0afacc456f0b524e722947106386c49a7a3
Mark Brough <mark.brough@gmail.com>
2021-05-07 19:50:13 +0200

    Add pagination for orgs list

--- pages/orgs/index.vue
+++ pages/orgs/index.vue
@@ -27,11 +27,53 @@
       </div>
       -->
       <p>
-        These are all the organisations associated with 3W and IATI
-        activities from 2020 forward on a single page. You can use
-        your browser search function to look for a specific
-        organisation.
+        The following organisations are associated with 3W and IATI
+        activities from 2020 onwards.
       </p>
+      <b-row class="mb-2">
+        <b-col lg="6" class="my-1">
+          <b-form-input
+            id="filter-input"
+            v-model="filter"
+            type="search"
+            placeholder="Type to search organisations..."
+          ></b-form-input>
+        </b-col>
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Organisations per page"
+            label-for="per-page"
+            label-cols-sm="5"
+            label-align-sm="right"
+            class="mb-0"
+          >
+            <b-form-select
+              id="per-page"
+              v-model="perPage"
+              :options="[100,200,500,1000]"
+            ></b-form-select>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row v-if="totalRows > (page*perPage)">
+        <b-col offset="md-6">
+          <b-form-group
+            label="Page"
+            label-cols-sm="5"
+            label-align-sm="right"
+            class="mb-0">
+            <b-pagination
+              v-model="page"
+              :total-rows="totalRows"
+              :per-page="perPage"
+              align="fill"
+              size="sm"
+              class="my-0"
+            ></b-pagination>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <hr />
       <section id="content">
         <div
           v-for='scope in ["local", "regional", "international", "unknown"]'
@@ -40,7 +82,7 @@
             <h2>{{ scope | scope | capitalize }}s</h2>
             <div class="inline-list">
               <Org
-                v-for="org in orgs"
+                v-for="org in filteredOrgs"
                 :key="org.info.name"
                 v-if="org.info.scope==scope"
                 :org="org"
@@ -61,13 +103,36 @@ import Org from '~/components/org.vue'
 export default {
   data() {
     return {
-      busy: true
+      busy: true,
+      perPage: 100,
+      filter: null,
+      page: 1
     }
   },
   components: {
     Org
   },
-  computed: mapState(['orgs']),
+  computed: {
+    filterStub() {
+      return this.filter.toLowerCase().replace(/[\W_]+/g,"-")
+    },
+    filterOrgs() {
+      const entries = Object.entries(this.orgs)
+      if ([null, ''].includes(this.filter)) { return entries }
+      return entries.filter(item => {
+        return item[0].includes(this.filterStub)
+      })
+    },
+    totalRows() {
+      return this.filterOrgs.length
+    },
+    filteredOrgs() {
+      return Object.fromEntries(
+        this.filterOrgs.slice((this.page-1)*this.perPage, this.perPage*(this.page)))
+    },...mapState(['orgs'])
+  },
+  methods: {
+  },
   async mounted() {
     await this.$store.dispatch('loadOrgs')
     this.busy = false
