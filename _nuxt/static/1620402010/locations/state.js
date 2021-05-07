1769f6ca252665836b9bf20f05c747719d40a3b1
markbrough <markbrough@users.noreply.github.com>
2021-05-07 15:40:13 +0000

    deploy: 177698c7d53dd3cfcc22a4fc39838a5236ca5fc2

---
+++ _nuxt/static/1620402010/locations/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fiati3w-web\u002F_nuxt\u002Fstatic\u002F1620402010",layout:"default",error:a,state:{orgs:{},sectors:{},activities:{},locations:{}},serverRendered:true,routePath:"\u002Flocations",config:{_app:{basePath:"\u002Fiati3w-web\u002F",assetsPath:"\u002Fiati3w-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
