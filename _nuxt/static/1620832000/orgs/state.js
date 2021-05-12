10282a155ee06154f31427db93954a4df92eaae5
markbrough <markbrough@users.noreply.github.com>
2021-05-12 15:06:43 +0000

    deploy: a27d529647f70d10aa8bcae422571cd94f72449c

---
+++ _nuxt/static/1620832000/orgs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fiati3w-web\u002F_nuxt\u002Fstatic\u002F1620832000",layout:"default",error:a,state:{orgs:{},sectors:{},activities:{},locations:{}},serverRendered:true,routePath:"\u002Forgs",config:{_app:{basePath:"\u002Fiati3w-web\u002F",assetsPath:"\u002Fiati3w-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
