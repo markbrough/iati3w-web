81f90bea99111cb61b5a78a8981e22479d19274e
Mark Brough <mark.brough@gmail.com>
2021-05-12 17:04:35 +0200

    Show number of organisations in tooltip and go to sector page when clicking on an element

--- components/treemap.vue
+++ components/treemap.vue
@@ -9,7 +9,7 @@ import Chart from 'chart.js';
 import 'chartjs-chart-treemap';
 export default {
   name: 'TreeMap',
-  props: ['data'],
+  props: ['data', 'type'],
   data() {
     return {
     }
@@ -19,52 +19,55 @@ export default {
       return Object.entries(this.data).map(item => {
         return {
           entry: item[1].name,
-          activities: item[1].activities.length
+          stub: item[1].stub,
+          activities: item[1].activities.length,
+          organisations: this.$options.filters.flatten(item[1].orgs).length
         }
       })
     }
   },
   methods: {
     drawChart() {
+      const datasets = [
+        {
+          label: 'Sectors',
+          tree: this.treeMapData,
+          key: "activities",
+          groups: ['entry'],
+          borderWidth: 1,
+          fontColor: "white",
+          borderColor: "rgba(255,255,255,1)",
+          //hoverBackgroundColor: "rgba(220,230,220,0.5)",
+          backgroundColor: [
+            '#CF3D1E',
+            '#F15623',
+            '#F68B1F',
+            '#FFC60B',
+            '#DFCE21',
+            '#BCD631',
+            '#95C93D',
+            '#48B85C',
+            '#00833D',
+            '#00B48D',
+            '#60C4B1',
+            '#27C4F4',
+            '#478DCB',
+            '#3E67B1',
+            '#4251A3',
+            '#59449B',
+            '#6E3F7C',
+            '#6A246D',
+            '#8A4873',
+            '#EB0080',
+            '#EF58A0',
+            '#C05A89'
+          ],
+        }
+      ]
       new Chart(this.$refs.treemapCanvas, {
         type: "treemap",
         data: {
-          datasets: [
-          {
-            label: 'Basic treemap',
-            tree: this.treeMapData,
-            key: "activities",
-            groups: ['entry'],
-            borderWidth: 1,
-            fontColor: "white",
-            borderColor: "rgba(200,200,200,1)",
-            //hoverBackgroundColor: "rgba(220,230,220,0.5)",
-            backgroundColor: [
-              '#CF3D1E',
-              '#F15623',
-              '#F68B1F',
-              '#FFC60B',
-              '#DFCE21',
-              '#BCD631',
-              '#95C93D',
-              '#48B85C',
-              '#00833D',
-              '#00B48D',
-              '#60C4B1',
-              '#27C4F4',
-              '#478DCB',
-              '#3E67B1',
-              '#4251A3',
-              '#59449B',
-              '#6E3F7C',
-              '#6A246D',
-              '#8A4873',
-              '#EB0080',
-              '#EF58A0',
-              '#C05A89'
-            ],
-          }
-          ]
+          datasets: datasets
         },
         options: {
           maintainAspectRatio: false,
@@ -83,10 +86,16 @@ export default {
             label: function(item, data) {
               var dataset = data.datasets[item.datasetIndex];
               var dataItem = dataset.data[item.index];
-              return dataset.key + ": " + dataItem.v;
+              const _d = dataItem._data.children[0]
+              return [` Activities: ${_d.activities}`, ` Organisations: ${_d.organisations}`]
               }
             }
-          }
+          },
+          onClick: ((c, i, d) => {
+            const type = this.type
+            const stub = datasets[0].data[i[0]._index]._data.children[0].stub
+            this.$router.push({name: 'sectors-type-stub', params: { type: type, stub: stub }})
+          })
         }
       })
     }
