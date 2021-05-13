a2b8b09d30dc7dd18c27252560b050c6b460605f
Mark Brough <mark.brough@gmail.com>
2021-05-13 13:41:39 +0200

    Move into collapsible for now and filter by source

--- components/orgs_list.vue
+++ components/orgs_list.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-row class="mb-2">
-      <b-col lg="12" class="my-1">
+      <b-col lg="9" class="my-1">
         <b-form-input
           id="filter-input"
           v-model="filter"
@@ -9,24 +9,41 @@
           placeholder="Type to search organisations..."
         ></b-form-input>
       </b-col>
+      <b-col class="text-md-right">
+        <b-form-radio-group
+          v-model="source"
+          :options="sourceOptions"
+          button-variant="outline-secondary"
+          name="radio-btn-outline"
+          buttons
+        ></b-form-radio-group>
+      </b-col>
     </b-row>
     <hr />
     <section id="content">
-      <div
-        v-for='scope in ["local", "regional", "international", "unknown"]'
-        :key="scope">
-        <section :id="scope">
-          <h2>{{ scope | scope | capitalize }}s</h2>
-          <div class="inline-list">
-            <Org
-              v-for="org in filteredOrgs"
-              :key="org.info.name"
-              v-if="org.info.scope==scope"
-              :org="org"
-              :activity_count="org.total_activities"
-              />
-          </div>
-        </section>
+      <div class="accordion" role="tablist">
+        <b-card no-body class="mb-1" :id="scope"
+          v-for='scope in ["local", "regional", "international", "unknown"]'
+          :key="scope">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button block v-b-toggle="`accordion-${scope}`">{{ scope | scope | capitalize }}s</b-button>
+          </b-card-header>
+          <b-collapse :id="`accordion-${scope}`" visible accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <b-card-text>
+                <div class="inline-list">
+                  <Org
+                    v-for="org in filteredOrgs"
+                    :key="org.info.name"
+                    v-if="org.info.scope==scope"
+                    :org="org"
+                    :activity_count="org.total_activities"
+                    />
+                </div>
+              </b-card-text>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
       </div>
     </section>
   </div>
@@ -37,6 +54,21 @@ export default {
   data() {
     return {
       filter: null,
+      source: 'both',
+      sourceOptions: [
+        {
+          value: 'IATI',
+          text: 'IATI'
+        },
+        {
+          value: '3W',
+          text: '3W'
+        },
+        {
+          value: 'both',
+          text: 'IATI + 3W'
+        }
+      ]
     }
   },
   components: {
@@ -45,17 +77,29 @@ export default {
   props: {
     orgs: Object
   },
+  methods: {
+    filterSource(item) {
+      if (this.source == 'both') { return true }
+      return item[1].sources.includes(this.source)
+    },
+    filterOrgs(item) {
+      if ([null, ''].includes(this.filter)) { return true }
+      return item[1].info.name.toLowerCase().includes(this.filter.toLowerCase())
+    },
+  },
   computed: {
-    filterOrgs() {
-      const entries = Object.entries(this.orgs)
-      if ([null, ''].includes(this.filter)) { return entries }
-      return entries.filter(item => {
-        return item[1].info.name.includes(this.filter)
-      })
+    entries() {
+      return Object.entries(this.orgs)
     },
     filteredOrgs() {
+      if (([null, ''].includes(this.filter)) && (this.source == 'both')) {
+        return this.orgs
+      }
       return Object.fromEntries(
-        this.filterOrgs)
+        this.entries.filter(entry => {
+          return this.filterOrgs(entry) && this.filterSource(entry)
+        })
+      )
     }
   },
   watch: {
