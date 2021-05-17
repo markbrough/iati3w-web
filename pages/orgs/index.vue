6b744b51fb55eb77b366fa1b25524bcc7a2ada9d
Mark Brough <mark.brough@gmail.com>
2021-05-18 00:17:41 +0200

    Add treemap to organisation page

--- pages/orgs/index.vue
+++ pages/orgs/index.vue
@@ -8,25 +8,28 @@
     <main v-else>
       <nav class="secondary">
         <nuxt-link
-            :to="{name: 'orgs', hash: '#local'}"
-        >Local and national actors</nuxt-link>
-        <nuxt-link
-            :to="{name: 'orgs', hash: '#regional'}"
-        >Regional actors</nuxt-link>
-        <nuxt-link
-            :to="{name: 'orgs', hash: '#international'}"
-        >International actors</nuxt-link>
-        <nuxt-link
-            :to="{name: 'orgs', hash: '#unknown'}"
-        >Undetermined actors</nuxt-link>
+          v-for="scope in Object.values(scopes)"
+          :key="scope.type"
+          :to="{name: 'orgs', hash: `#${scope.type}`}"
+        >{{ scope.name }}</nuxt-link>
       </nav>
       <h1>Aid organisations active in Somalia</h1>
       <p>
         The following organisations are associated with 3W and IATI
         activities from 2020 onwards.
       </p>
+
+      <client-only>
+        <Treemap
+          :data="orgsSummary"
+          summarise-by="organisations"
+          class="mb-4"
+          :clickable="false"
+          :colors="orgTypeColors" />
+      </client-only>
       <OrgsList
-        :orgs="orgs" />
+        :orgs="orgs"
+        :source.sync="source" />
     </main>
 
   </div>
@@ -35,16 +38,80 @@
 <script>
 import { mapState } from 'vuex'
 import OrgsList from '~/components/orgs_list.vue'
+import Treemap from '~/components/treemap.vue'
 export default {
   data() {
     return {
-      busy: true
+      busy: true,
+      scopes: {
+        'local': {
+          'name': 'Local and national actors',
+          'type': 'local',
+          'organisations': 0
+        },
+        'regional': {
+          'name': 'Regional actors',
+          'type': 'regional',
+          'organisations': 0
+        },
+        'international': {
+          'name': 'International actors',
+          'type': 'international',
+          'organisations': 0
+        },
+        'unknown': {
+          'name': 'Undetermined actors',
+          'type': 'unknown',
+          'organisations': 0
+        }
+      },
+      source: 'all',
+      orgTypeColors: {
+        'Local and national actors': '#CF3D1E',
+        'Regional actors': '#F15623',
+        'International actors': '#F68B1F',
+        'Undetermined actors': '#FFC60B'
+      }
     }
   },
   components: {
-    OrgsList
+    OrgsList, Treemap
   },
-  computed: mapState(['orgs']),
+  computed: {
+    orgsSummary() {
+      return Object.values(
+        Object.values(this.orgs).reduce((summary, item) => {
+          if (this.source == 'all') {
+            summary[item.info.scope].organisations += 1
+          } else if (item.sources.includes(this.source)) {
+            summary[item.info.scope].organisations += 1
+          }
+          return summary
+        }, {
+          'local': {
+            'name': 'Local and national actors',
+            'type': 'local',
+            'organisations': 0
+          },
+          'regional': {
+            'name': 'Regional actors',
+            'type': 'regional',
+            'organisations': 0
+          },
+          'international': {
+            'name': 'International actors',
+            'type': 'international',
+            'organisations': 0
+          },
+          'unknown': {
+            'name': 'Undetermined actors',
+            'type': 'unknown',
+            'organisations': 0
+          }
+        })
+      )
+    },...mapState(['orgs']),
+  },
   methods: {
   },
   async mounted() {
