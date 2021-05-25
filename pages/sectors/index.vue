3e95359a3890903c63d74b1d381a5c38cc0fafae
Mark Brough <mark.brough@gmail.com>
2021-05-25 14:00:59 +0100

    Update sectors page

--- pages/sectors/index.vue
+++ pages/sectors/index.vue
@@ -6,18 +6,9 @@
       </div>
     </main>
     <main v-else>
-      <nav class="secondary">
-        <nuxt-link
-            :to="{name: 'sectors', hash: '#humanitarian'}"
-          >Humanitarian</nuxt-link>
-        <nuxt-link
-            :to="{name: 'sectors', hash: '#dac'}"
-          >OECD</nuxt-link>
-      </nav>
-      <h1>Aid clusters and sectors in Somalia</h1>
+      <h1>Sectors in Somalia</h1>
       <section id="content">
         <section id="humanitarian">
-          <h3>Humanitarian clusters and areas of concern</h3>
           <client-only>
             <Treemap
               :data="sectors.humanitarian"
@@ -33,26 +24,6 @@
               type="humanitarian"
               :org_count="flatten(sectors.humanitarian[stub].orgs).length"
               :activity_count="sectors.humanitarian[stub].activities.length" />
-          </div>
-        </section>
-        <hr />
-        <section id="dac">
-          <h3>OECD-DAC sectors</h3>
-          <client-only>
-            <Treemap
-              :data="sectors.dac"
-              type="dac"
-              class="mb-4" />
-          </client-only>
-          <div class="inline-list">
-            <Sector
-              v-for="stub in Object.keys(sectors.dac).sort()"
-              :key="stub"
-              :name="sectors.dac[stub].name"
-              :stub="stub"
-              type="dac"
-              :org_count="flatten(sectors.dac[stub].orgs).length"
-              :activity_count="sectors.dac[stub].activities.length" />
           </div>
         </section>
       </section>
