e7a3fca2d5453ca3a00dcf4236c6282299a500f6
markbrough <markbrough@users.noreply.github.com>
2022-02-21 09:56:24 +0000

    deploy: 897bbee4605030c736e8871a7ac7505274563c15

--- _nuxt/static/1622207978/network/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622207978",layout:"default",error:a,state:{orgs:{},sectors:{},activities:{},locations:{},geoJSONData:{},source:"all"},serverRendered:true,routePath:"\u002Fnetwork",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
